2edee2056478fdbea7c66f4c5271c9f77d31af61
rikkamus <rick@rikkamus.com>
2025-07-27 12:37:17 +0200

    Initial commit

---
+++ src/main/java/com/rikkamus/craftersoneclaimvisualizer/ClaimVisualizerMod.java
@@ -0,0 +1,17 @@
+package com.rikkamus.craftersoneclaimvisualizer;
+
+import org.slf4j.Logger;
+import com.mojang.logging.LogUtils;
+import net.neoforged.fml.common.Mod;
+
+@Mod(ClaimVisualizerMod.MOD_ID)
+public class ClaimVisualizerMod {
+
+    public static final String MOD_ID = "craftersoneclaimvisualizer";
+    public static final String MOD_VERSION = "0.1.0";
+
+    public ClaimVisualizerMod() {
+
+    }
+
+}
