a599a437e1df5f9b7a50ba6ff3f5968f03063c1b
rikkamus <rick@rikkamus.com>
2026-06-11 16:14:40 +0200

    Switch from Jackson to JSON Java

--- common/src/main/java/com/rikkamus/craftersoneclaimvisualizer/claim/Claim.java
+++ common/src/main/java/com/rikkamus/craftersoneclaimvisualizer/claim/Claim.java
@@ -1,111 +1,27 @@
 package com.rikkamus.craftersoneclaimvisualizer.claim;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.rikkamus.craftersoneclaimvisualizer.ChatLogger;
-import com.rikkamus.craftersoneclaimvisualizer.ClaimVisualizerMod;
-import com.rikkamus.craftersoneclaimvisualizer.Color;
-import com.rikkamus.craftersoneclaimvisualizer.geometry.PolygonUtil;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
-import net.minecraft.ChatFormatting;
+import lombok.*;
 import org.jetbrains.annotations.NotNull;
-import org.joml.Vector2i;
 import org.joml.Vector3f;
-import org.joml.Vector4f;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 @Getter
 @Setter
-@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
 public class Claim {
 
-    @JsonProperty("claimID")
     private String claimId;
-
     private String owner;
-    private List<String> collaborators;
     private String type;
-    private String description;
-
-    @JsonIgnore
     private Vector3f rgb;
 
     @NotNull
-    @JsonIgnore
     @Setter(AccessLevel.NONE)
     private Polygon rawShape;
 
     @NotNull
-    @JsonIgnore
     @Setter(AccessLevel.NONE)
     private Polygon correctedShape;
-
-    @JsonCreator
-    public Claim(@JsonProperty("claimID") String claimId,
-                 @JsonProperty("owner") String owner,
-                 @JsonProperty("collaborators") List<String> collaborators,
-                 @JsonProperty("type") String type,
-                 @JsonProperty("description") String description,
-                 @JsonProperty("color") String color,
-                 @JsonProperty(value = "coords", required = true) int[][] rawCoords) {
-        this.claimId = claimId;
-        this.owner = owner;
-        this.collaborators = collaborators;
-        this.type = type;
-        this.description = description;
-        setColor(color);
-        setRawCoords(rawCoords);
-    }
-
-    @JsonProperty("color")
-    public String getColor() {
-        return Color.toRgbaHex(new Vector4f(this.rgb.x, this.rgb.y, this.rgb.z, 1.0f));
-    }
-
-    public void setColor(String color) {
-        this.rgb = color != null ? Color.parseRgbaHex(color).xyz(new Vector3f()) : null;
-    }
-
-    @JsonProperty(value = "coords")
-    public int[][] getRawCoords() {
-        int[][] rawCoords = new int[this.rawShape.npoints][2];
-
-        for (int i = 0; i < this.rawShape.npoints; i++) {
-            rawCoords[i][0] = this.rawShape.xpoints[i];
-            rawCoords[i][1] = this.rawShape.ypoints[i];
-        }
-
-        return rawCoords;
-    }
-
-    public void setRawCoords(int[][] rawCoords) {
-        Objects.requireNonNull(rawCoords);
-        List<Vector2i> vectors = new ArrayList<>(rawCoords.length);
-
-        for (int i = 0; i < rawCoords.length; i++) {
-            if (rawCoords[i].length != 2) throw new IllegalArgumentException("Polygon points require exactly two coordinates.");
-            vectors.add(new Vector2i(rawCoords[i][0], rawCoords[i][1]));
-        }
-
-        this.rawShape = PolygonUtil.createPolygonFromRawPoints(vectors);
-
-        try {
-            this.correctedShape = PolygonUtil.createPolygonFromBlockPoints(vectors);
-        } catch (Exception e) {
-            String errorMessage = String.format("Failed to create corrected claim shape for claim with ID \"%s\". Uncorrected shape will be used instead.", this.claimId);
-            ClaimVisualizerMod.LOGGER.error(errorMessage, e);
-            ChatLogger.log(errorMessage, ChatFormatting.RED);
-
-            this.correctedShape = this.rawShape;
-        }
-    }
 
 }
--- common/src/main/java/com/rikkamus/craftersoneclaimvisualizer/claim/ClaimRepository.java
+++ common/src/main/java/com/rikkamus/craftersoneclaimvisualizer/claim/ClaimRepository.java
@@ -1,13 +1,23 @@
 package com.rikkamus.craftersoneclaimvisualizer.claim;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rikkamus.craftersoneclaimvisualizer.ChatLogger;
+import com.rikkamus.craftersoneclaimvisualizer.ClaimVisualizerMod;
+import com.rikkamus.craftersoneclaimvisualizer.Color;
+import com.rikkamus.craftersoneclaimvisualizer.geometry.PolygonUtil;
+import net.minecraft.ChatFormatting;
+import org.joml.Vector2i;
+import org.joml.Vector3f;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.awt.*;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpTimeoutException;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -19,11 +29,42 @@ public class ClaimRepository implements AutoCloseable {
         return responseCode >= 200 && responseCode < 300;
     }
 
+    private static Claim parseClaim(JSONObject jsonClaim) {
+        String claimId = jsonClaim.has("claimID") ? jsonClaim.getString("claimID") : null;
+        String owner = jsonClaim.has("owner") ? jsonClaim.getString("owner") : null;
+        String type = jsonClaim.has("type") ? jsonClaim.getString("type") : null;
+        Vector3f rgb = jsonClaim.has("color") ? Color.parseRgbaHex(jsonClaim.getString("color")).xyz(new Vector3f()) : null;
+
+        JSONArray jsonPoints = jsonClaim.getJSONArray("coords");
+        List<Vector2i> points = new ArrayList<>(jsonPoints.length());
+
+        for (int i = 0; i < jsonPoints.length(); i++) {
+            JSONArray jsonPoint = jsonPoints.getJSONArray(i);
+            if (jsonPoint.length() != 2) throw new IllegalArgumentException("Invalid claim points.");
+
+            points.add(new Vector2i(jsonPoint.getInt(0), jsonPoint.getInt(1)));
+        }
+
+        Polygon rawShape = PolygonUtil.createPolygonFromRawPoints(points);
+        Polygon correctedShape;
+
+        try {
+            correctedShape = PolygonUtil.createPolygonFromBlockPoints(points);
+        } catch (Exception e) {
+            String errorMessage = String.format("Failed to create corrected claim shape for claim with ID \"%s\". Uncorrected shape will be used instead.", claimId);
+            ClaimVisualizerMod.LOGGER.error(errorMessage, e);
+            ChatLogger.log(errorMessage, ChatFormatting.RED);
+
+            correctedShape = rawShape;
+        }
+
+        return new Claim(claimId, owner, type, rgb, rawShape, correctedShape);
+    }
+
     private final URI endpointUri;
     private final String userAgent;
     private final Duration timeout;
     private final HttpClient client;
-    private final ObjectMapper objectMapper;
 
     public ClaimRepository(URI endpointUri, String userAgent, Duration timeout) {
         this.endpointUri = endpointUri;
@@ -33,7 +74,6 @@ public class ClaimRepository implements AutoCloseable {
                                 .connectTimeout(timeout)
                                 .followRedirects(HttpClient.Redirect.NORMAL)
                                 .build();
-        this.objectMapper = new ObjectMapper();
     }
 
     public CompletableFuture<Collection<Claim>> findAllClaims() {
@@ -56,7 +96,14 @@ public class ClaimRepository implements AutoCloseable {
             }
 
             try {
-                return this.objectMapper.readValue(response.body(), this.objectMapper.getTypeFactory().constructCollectionType(List.class, Claim.class));
+                JSONArray jsonClaims = new JSONArray(response.body());
+                List<Claim> claims = new ArrayList<>(jsonClaims.length());
+
+                for (int i = 0; i < jsonClaims.length(); i++) {
+                    claims.add(ClaimRepository.parseClaim(jsonClaims.getJSONObject(i)));
+                }
+
+                return claims;
             } catch (Exception e) {
                 throw new RepositoryFetchException("Failed to parse response body.", e);
             }
